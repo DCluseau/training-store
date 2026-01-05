0498f878a825e48d716a2c8824d7b012fadf8906
Daphne Cluseau <daphne.cluseau@fms-ea.com>
2026-01-05 09:25:51 +0100

    Init project, added main program

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+/**
+ * Application
+ */
+public class Main {
+	/**
+	 * Constructor
+	 */
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * Main program
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
