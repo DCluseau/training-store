60f22f12876715f6ed68136f29a84254a2a56d2f
Daphne Cluseau <daphne.cluseau@fms-ea.com>
2026-01-05 15:13:52 +0100

    Init class

---
+++ src/models/Category.java
@@ -0,0 +1,48 @@
+package models;
+
+public class Category {
+
+	private int id;
+	private String name;
+	
+	public Category() {
+		this.id = 0;
+		this.name = "";
+	}
+	
+	public Category(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return this.id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
