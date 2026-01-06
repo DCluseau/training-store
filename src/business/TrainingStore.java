6825de289d6707d2216429a8cfe8565855264dd8
Daphne Cluseau <daphne.cluseau@fms-ea.com>
2026-01-06 11:10:42 +0100

    Init TrainingStore to use classes in main

---
+++ src/business/TrainingStore.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package business;
+
+/**
+ * Class to use all class objects
+ */
+public class TrainingStore {
+
+	/**
+	 * Constructor without parameters
+	 */
+	public TrainingStore() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
