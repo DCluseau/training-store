39e6c3fd02ab1e2db1dcc5eb94e8529f54a745ff
Daphne Cluseau <daphne.cluseau@fms-ea.com>
2026-01-06 08:50:02 +0100

    Added javadoc

--- src/daos/IDao.java
+++ src/daos/IDao.java
@@ -1,12 +1,33 @@
 package daos;
 
+import java.sql.ResultSet;
+
+/**
+ * Interface IDao
+ */
 public interface IDao {
 	
+	/**
+	 * Create record in database
+	 * @return
+	 */
 	public boolean create();
 	
-	public void read();
+	/**
+	 * Read one record
+	 * @return
+	 */
+	public ResultSet read();
 	
+	/**
+	 * Update one record
+	 * @return
+	 */
 	public boolean update();
 	
+	/**
+	 * Delete one record
+	 * @return
+	 */
 	public boolean delete();
 }
